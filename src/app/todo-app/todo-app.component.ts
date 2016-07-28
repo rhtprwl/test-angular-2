965407f817abee8607571a89b79e39baf1f29f1f
anjain <ankur.jain@ongraph.com>
2016-07-28 18:02:20 +0530

    todo app changes

---
+++ src/app/todo-app/todo-app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+// Import class so we can register it as dependency injection token
+import {Todo} from '../todo';
+import {TodoService} from '../todo.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'todo-app',
+  templateUrl: 'todo-app.component.html',
+  styleUrls: ['todo-app.component.css'],
+  providers: [TodoService]
+})
+export class TodoAppComponent {
+
+  newTodo: Todo = new Todo();
+
+  constructor(private todoService: TodoService) {
+  }
+
+  addTodo() {
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoService.getAllTodos();
+  }
+
+}
